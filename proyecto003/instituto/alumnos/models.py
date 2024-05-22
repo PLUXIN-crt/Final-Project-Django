61f58d49d1dae657f32cfa2c5225a0fe58f3038c
PLUXIN <pluxin485@gmail.com>
2024-05-22 11:17:44 -0400

    add django

--- proyecto003/instituto/alumnos/models.py
+++ proyecto003/instituto/alumnos/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class Escuela(models.Model):
+    nombre = models.CharField(max_length=50)
+    activo = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Carrera(models.Model):
+    escuela = models.ForeignKey(Escuela, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    version = models.IntegerField()    
+    activo = models.IntegerField()
+
+    def __str__(self):
+        return self.escuela.nombre + ' - ' + self.nombre
+
+
+
+
+
+
+
+class Alumno(models.Model):
+    rut   = models.CharField(primary_key=True, max_length=10)
+    dv   = models.CharField(max_length=1)
+    nombre = models.CharField(max_length=50)
+    apellido1 = models.CharField(max_length=50)
+    apellido2 = models.CharField(max_length=50)
+    nacimiento = models.DateField()
+    fono = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=200)
+    email = models.CharField(max_length=100)
+    activo = models.IntegerField()
+
+def __str__(self):
+    return self.rut , '-',self.dv 
